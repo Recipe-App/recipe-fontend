f05319b4e40600ddf69f29ec051e67461bf1ac8d
learn <learn@learn12.local>
2018-08-01 10:43:40 -0700

    Login form works! Server returns a JWT. NOT TESTED

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Register from './pages/register.js';
+import Login from './pages/login'
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Login />
         <Register />
       </div>
     );
---
+++ src/pages/login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import LoginForm from '../components/login'
+
+class Login extends Component {
+    render() {
+        return(
+            <LoginForm />
+        )
+    }
+}
+
+export default Login
