8b26f99c2db3fac68e7bc6c9f8c5d95d8c296fbb
learn <learn@learn14.local>
2018-08-03 10:32:56 -0700

    Created new recipes component that allows the user to save individual recipes into their saved recipes page

--- src/api/index.js
+++ src/api/index.js
@@ -17,6 +17,7 @@
 // }
 //
 // export default createUser
+
 require('dotenv').config()
 
 let getRecipes = (pantry) => {
--- src/components/new_recipes.js
+++ src/components/new_recipes.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import getRecipes from '../api/index'
+import { Image, Button } from 'react-bootstrap'
 
 // require('dotenv').config()
 
@@ -10,7 +11,8 @@ class NewRecipes extends Component {
         this.state = {
             apiResp: {
                 recipes: []
-            }
+            },
+            saved: []
         }
     }
 
@@ -25,25 +27,43 @@ class NewRecipes extends Component {
           })
     }
 
+    handleClick(event){
+
+        let id = event.target.id
+        console.log(id);
+        let { saved } = this.state
+        let { recipes } = this.state.apiResp
+
+        let savedUrls = saved.map((element) => {
+          return element.recipe.url
+        })
+
+        if(!savedUrls.includes(recipes[id].recipe.url)){
+          saved.push(recipes[id])
+          this.setState({ saved: saved })
+        }
+
+        console.log(this.state.saved);
+    }
+
     render() {
-      console.log(this.state.apiResp.recipes)
         return(
           <div>
-          {this.state.apiResp.recipes.map((element) => {
+          {this.state.apiResp.recipes.map((element, index) => {
             return(
-              <div onClick={this.handleClick.bind(this)}>
-              <img src={element.recipe.image}/>
+              <form onClick={this.handleClick.bind(this)}>
+              <Image src={element.recipe.image} circle/>
               <h1> {element.recipe.label} </h1>
               <a href={element.recipe.url}>{element.recipe.url}</a>
               {element.recipe.ingredients.map((elementTwo) =>{
                 return(
                   <div>
                   <h1> {elementTwo.text} </h1>
-
                   </div>
                 )
               })}
-              </div>
+              <Button id={`${index}`} bsStyle="danger">Save Recipe</Button>
+              </form>
             )
           })}
           </div>
