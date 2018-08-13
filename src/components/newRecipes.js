688d8b372937214bc67c345166115d8ce7bcd05b
learn <learn@learn14.local>
2018-08-13 16:09:37 -0700

    Made the buttons on the new recipe page responsive.  It doesn't work if you click into saved recipes and go back to new recipes

--- src/components/newRecipes.js
+++ src/components/newRecipes.js
@@ -14,9 +14,7 @@ class NewRecipes extends Component {
             apiResp: {
                 recipes: []
             },
-            saved: {
-              recipes:[]
-            },
+            saved: [],
             clicked: [false,false,false,false,false,false,false,false,false,false]
         }
     }
@@ -39,9 +37,10 @@ class NewRecipes extends Component {
 
     handleClick(event){  // It adds the clicked recipe into state and prevents duplicate entries
 
-        let id = event.target.id.split(',')  //"id" is an array of two strings.  id[0] is equal to the index of the button.  id[1] is equal to the recipe uri for the corresponding button.
+        let id = event.target.id.split(',')  //"id" is an array of two strings.  id[0] is equal to the index of the button.  id[1] is equal to the recipe url for the corresponding button.
         // console.log("This is the id of ");
         let index = parseInt(id[0])
+        let url = id[1]
         // console.log("This is the index of the clicked recipe: ", index);
         // console.log("Which is a ", typeof index);
         let { saved } = this.state
@@ -51,7 +50,7 @@ class NewRecipes extends Component {
         clicked[index] = !clicked[index] //The button state is changed
         // console.log("This is the recipes array: ", recipes);
 
-        let recipeObj = recipes.filter(resp => resp.recipe.uri === id[1])[0]  //recipeObj is the first element of a 1 element array of a sincle recipe.  recipeObj = [{...}][0] = {...}
+        let recipeObj = recipes.filter(resp => resp.recipe.url === id[1])[0]  //recipeObj is the first element of a 1 element array of a sincle recipe.  recipeObj = [{...}][0] = {...}
 
         if (clicked[index]){
           console.log("This is the clicked array: ", clicked);
@@ -59,11 +58,23 @@ class NewRecipes extends Component {
           // console.log(recipeObj);
           let toSave = {recipe: this.processRecipe(recipeObj.recipe)}  //This method works
           saveRecipes(toSave)
-
-          this.setState({ clicked })
+              .then( resp => {
+                  let { saved } = this.state
+                  saved.push(resp)
+                  this.setState({ saved, clicked })
+              })
 
         } else {
-              deleteRecipe(recipeObj.recipe.url)
+              let { saved } = this.state
+
+              let toDelete = saved.filter( recipe => recipe.url === url )[0]
+
+              saved = saved.filter( recipe => recipe.url !== url )
+
+              deleteRecipe(toDelete.id)
+
+              this.setState({ saved, clicked })
+
         }
 
     }
@@ -94,6 +105,7 @@ class NewRecipes extends Component {
 
     render() {
       console.log("This is the clicked array from state: ", this.state.clicked);
+      console.log("This is the saved array from state", this.state.saved);
         return(
           <div className="flex-container">
           {this.state.apiResp.recipes.map( (element, index) => {
@@ -104,7 +116,7 @@ class NewRecipes extends Component {
                </Panel.Heading>
                <Panel.Collapse>
                <Panel.Body collapsible>
-               <ButtonFunction id={`${index},${element.recipe.uri}`} onClick= {this.handleClick.bind(this)} style={!this.state.clicked[index] ? "danger" : "warning"} text={!this.state.clicked[index] ? "Save Recipe" : "Unsave Recipe"}/>
+               <ButtonFunction id={`${index},${element.recipe.url}`} onClick= {this.handleClick.bind(this)} style={!this.state.clicked[index] ? "danger" : "warning"} text={!this.state.clicked[index] ? "Save Recipe" : "Unsave Recipe"}/>
                     <h3>
                         <a className="title" href={element.recipe.url} target="_blank">{element.recipe.label}</a>
                     </h3>
