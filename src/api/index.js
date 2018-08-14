b69fac8f3fe3dd2ef0b9019d3572c6f650af721f
learn <learn@learn14.local>
2018-08-14 15:45:06 -0700

    Updated routing and fixed domain

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import AuthService from '../services/AuthService'
 
-const DOMAIN = "https://the-recipe-app.herokuapp.com/"
+const DOMAIN = "https://the-recipe-app.herokuapp.com"
 const Auth = new AuthService()
 
 
