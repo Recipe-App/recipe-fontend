4f0580b77e947f3af756c1345152e00bab8ef005
learn <learn@learn14.local>
2018-08-16 12:20:05 -0700

    Migrated all logic from savedRecipes to saved

--- src/components/savedRecipes.js
+++ src/components/savedRecipes.js
@@ -7,16 +7,14 @@ const tooltip = ( <Tooltip id="tooltip"> remove </Tooltip> )
 
 class SavedRecipes extends Component {
 
-  constructor(props){
-    super(props)
-
-    this.state = {
-        ids: sessionStorage.getItem('ids'),
-        show: false,
-        groceryList: []
-
-    }
-  }
+  // constructor(props){
+  //   super(props)
+  //
+  //   this.state = {
+  //       ids: sessionStorage.getItem('ids'),
+  //       show: false,
+  //   }
+  // }
 
   handleSubmit = () => {
 
@@ -27,71 +25,71 @@ class SavedRecipes extends Component {
     })
   }
 
-  handleClose = () => {
-      this.setState({ show: false });
-  }
+  // handleClose = () => {
+  //     this.setState({ show: false });
+  // }
+  //
+  // handleShow = () => {
+  //     this.setState({ show: true });
+  // }
 
-  handleShow = () => {
-      this.setState({ show: true });
-  }
+  // handleClear = () => {
+  //   let ids = "placeholder"
+  //   let groceryList = []
+  //   this.setState({ ids, groceryList })
+  // }
 
-  handleClear = () => {
-    let ids = "placeholder"
-    let groceryList = []
-    this.setState({ ids, groceryList })
-  }
+  // handleRemove = (event) => {
+  //   let toRemove = String(event.target.id) //This is the button id that corresponds to a certin recipe
+  //   let groceryList
+  //
+  //   if (!this.state.ids.split("").includes(',')) { //When there is only one recipe left
+  //       sessionStorage.setItem('ids', "placeholder")
+  //       groceryList = []
+  //
+  //   } else {  //When there is more than one recipe left
+  //
+  //         let updatedIds = this.state.ids.split(',').filter( id => id !== toRemove )  //This filter allows all ids in the ids array to pass unless it matches the id that is to be removed
+  //         sessionStorage.setItem('ids', updatedIds)  //The updated ids are saved in session storage and available in state
+  //
+  //         groceryList = this.props.saved.filter(( savedRecipe => updatedIds.includes(String(savedRecipe.id))))
+  //
+  //   }
+  //
+  //   let ids = sessionStorage.getItem('ids')
+  //
+  //   this.setState({ ids, groceryList })
+  //
+  // }
 
-  handleRemove = (event) => {
-    let toRemove = String(event.target.id) //This is the button id that corresponds to a certin recipe
-    let groceryList
+  // handleAdd = (event) => {
+  //   let id = event.target.id  //This is the numeric id for the button that was clicked, which corresponds to the recipe id
+  //   let ids =  this.state.ids //create a copy of the ids in state
+  //   let { groceryList } = this.state
+  //   let saved = this.props.saved
+  //
+  //   if (ids === "placeholder") {  //If the session just has the placeholder
+  //       ids = id
+  //       groceryList = saved.filter(( savedRecipe => savedRecipe.id == ids ))  //Here ids should just have one number
+  //   } else if (ids !== id){  //If there is just one id
+  //         ids = ids + ',' + id
+  //         groceryList = saved.filter(( savedRecipe => ids.split(',').includes(String(savedRecipe.id))))  //Here ids should have more than
+  //   } else if (!ids.split(',').includes(id)) {  //If there are already many ids
+  //         ids = ids + ',' + id;
+  //         groceryList = saved.filter(( savedRecipe => ids.split(',').includes(String(savedRecipe.id))))  //Here ids should have more than
+  //   }
+  //
+  //   sessionStorage.setItem("ids", ids)  //Save filtered ids to session storage.  This will be made available in state
+  //
+  //   this.setState({ ids: sessionStorage.getItem('ids'),
+  //                   groceryList: groceryList })
+  //
+  // }
 
-    if (!this.state.ids.split("").includes(',')) { //When there is only one recipe left
-        sessionStorage.setItem('ids', "placeholder")
-        groceryList = []
-
-    } else {  //When there is more than one recipe left
-
-          let updatedIds = this.state.ids.split(',').filter( id => id !== toRemove )  //This filter allows all ids in the ids array to pass unless it matches the id that is to be removed
-          sessionStorage.setItem('ids', updatedIds)  //The updated ids are saved in session storage and available in state
-
-          groceryList = this.props.saved.filter(( savedRecipe => updatedIds.includes(String(savedRecipe.id))))
-
-    }
-
-    let ids = sessionStorage.getItem('ids')
-
-    this.setState({ ids, groceryList })
-
-  }
-
-  handleAdd = (event) => {
-    let id = event.target.id  //This is the numeric id for the button that was clicked, which corresponds to the recipe id
-    let ids =  this.state.ids //create a copy of the ids in state
-    let { groceryList } = this.state
-    let saved = this.props.saved
-
-    if (ids === "placeholder") {  //If the session just has the placeholder
-        ids = id
-        groceryList = saved.filter(( savedRecipe => savedRecipe.id == ids ))  //Here ids should just have one number
-    } else if (ids !== id){  //If there is just one id
-          ids = ids + ',' + id
-          groceryList = saved.filter(( savedRecipe => ids.split(',').includes(String(savedRecipe.id))))  //Here ids should have more than
-    } else if (!ids.split(',').includes(id)) {  //If there are already many ids
-          ids = ids + ',' + id;
-          groceryList = saved.filter(( savedRecipe => ids.split(',').includes(String(savedRecipe.id))))  //Here ids should have more than
-    }
-
-    sessionStorage.setItem("ids", ids)  //Save filtered ids to session storage.  This will be made available in state
-
-    this.setState({ ids: sessionStorage.getItem('ids'),
-                    groceryList: groceryList })
-
-  }
-
-  handleDelete = (event) => {
-    deleteRecipe(event.target.id)
-    window.location.reload(true)
-  }
+  // handleDelete = (event) => {
+  //   deleteRecipe(event.target.id)
+  //   window.location.reload(true)
+  // }
 
 
   render() {
@@ -101,24 +99,24 @@ class SavedRecipes extends Component {
 
         <div className="flex-container">
 
-        <Button className="list_style" bsSize="large" onClick={this.handleShow}>
+        <Button className="list_style" bsSize="large" onClick={this.props.handleShow}>
         See Grocery List
         </Button>
 
-        <Modal show={this.state.show} onHide={this.handleClose} bsSize="small">
+        <Modal show={this.props.show} onHide={this.props.handleClose} bsSize="small">
             <Modal.Header closeButton>
                 <Modal.Title>Shopping List</Modal.Title>
             </Modal.Header>
 
             <Modal.Body>
-            { this.state.ids == "placeholder" ?
+            { this.props.ids == "placeholder" ?
 
                   <p>There are no ingredients in the shopping list </p>
 
               :   this.props.saved.map((recipe) => {
                       return (
                         <div>
-                            <OverlayTrigger  onClick={this.handleRemove} placement="right" overlay={tooltip}>
+                            <OverlayTrigger  onClick={this.props.handleRemove} placement="right" overlay={tooltip}>
                                 <div>
                                     <h5 id={`${recipe.id}`}>{recipe.label}</h5>
                                 </div>
@@ -127,7 +125,7 @@ class SavedRecipes extends Component {
                             <ul>
                             {recipe.ingredients.split('//').map((item) => {
                               return (
-                                <li>Item</li>
+                                <li>{ item }</li>
                               )
                             })}
                             </ul>
@@ -140,8 +138,8 @@ class SavedRecipes extends Component {
             }
             </Modal.Body>
             <Modal.Footer>
-                <Button onClick={this.handleClear}>Clear</Button>
-                <Button onClick={this.handleSubmit}>Text Me</Button>
+                <Button onClick={this.props.handleClear}>Clear</Button>
+                <Button onClick={this.props.handleSubmit}>Text Me</Button>
             </Modal.Footer>
         </Modal>
 
@@ -154,9 +152,9 @@ class SavedRecipes extends Component {
                       </Panel.Heading>
 
                       <Panel.Body>
-                          <Button id={`${recipe.id}`} bsStyle="danger" className="button" onClick={this.handleDelete.bind(this)}>Unsave</Button>
+                          <Button id={`${recipe.id}`} bsStyle="danger" className="button" onClick={this.props.handleDelete}>Unsave</Button>
 
-                          <Button id={`${recipe.id}`} bsStyle="success"  className="button" onClick={this.handleAdd.bind(this)}>Add To Grocery List</Button><br/><br/>
+                          <Button id={`${recipe.id}`} bsStyle="success"  className="button" onClick={this.props.handleAdd}>Add To Grocery List</Button><br/><br/>
 
                           <h3>
                           <a href={recipe.url} className="title" target="_blank">{recipe.label}</a>
--- src/pages/saved.js
+++ src/pages/saved.js
@@ -10,8 +10,84 @@ class Saved extends Component {
   constructor(props){
     super(props)
     this.state = {
-      saved: []
+      saved: [],
+      ids: sessionStorage.getItem('ids'),
+      show: false,
+      groceryList: []
     }
+  }
+
+  handleClose = () => {
+      this.setState({ show: false });
+  }
+
+  handleShow = () => {
+      this.setState({ show: true });
+  }
+
+  handleClear = () => {
+    sessionStorage.setItem('ids', 'placeholder')
+    let ids = sessionStorage.getItem('ids')
+    let groceryList = []
+    this.setState({ ids, groceryList })
+  }
+
+
+  handleAdd = (event) => {
+    console.log("I'm Added!");
+    let id = event.target.id  //This is the numeric id for the button that was clicked, which corresponds to the recipe id
+    let ids =  this.state.ids //create a copy of the ids in state
+    let { groceryList } = this.state
+    let saved = this.state.saved
+
+    if (ids === "placeholder") {  //If the session just has the placeholder
+        ids = id
+        groceryList = saved.filter(( savedRecipe => savedRecipe.id == ids ))  //Here ids should just have one number
+    } else if (ids !== id){  //If there is just one id
+          ids = ids + ',' + id
+          groceryList = saved.filter(( savedRecipe => ids.split(',').includes(String(savedRecipe.id))))  //Here ids should have more than
+    } else if (!ids.split(',').includes(id)) {  //If there are already many ids
+          ids = ids + ',' + id;
+          groceryList = saved.filter(( savedRecipe => ids.split(',').includes(String(savedRecipe.id))))  //Here ids should have more than
+    }
+
+    sessionStorage.setItem("ids", ids)  //Save filtered ids to session storage.  This will be made available in state
+
+    this.setState({ ids: sessionStorage.getItem('ids'),
+                    groceryList: groceryList })
+
+  }
+
+  handleRemove = (event) => {
+    let toRemove = String(event.target.id) //This is the button id that corresponds to a certin recipe
+    let groceryList
+
+    if (!this.state.ids.split("").includes(',')) { //When there is only one recipe left
+        sessionStorage.setItem('ids', "placeholder")
+        groceryList = []
+
+    } else {  //When there is more than one recipe left
+
+          let updatedIds = this.state.ids.split(',').filter( id => id !== toRemove )  //This filter allows all ids in the ids array to pass unless it matches the id that is to be removed
+          sessionStorage.setItem('ids', updatedIds)  //The updated ids are saved in session storage and available in state
+
+          groceryList = this.props.saved.filter(( savedRecipe => updatedIds.includes(String(savedRecipe.id))))
+
+    }
+
+    let ids = sessionStorage.getItem('ids')
+
+    this.setState({ ids, groceryList })
+
+  }
+
+  handleDelete = (event) => {
+    deleteRecipe(event.target.id)
+    getSaved()
+      .then( res =>
+        { let saved = this.state
+          saved = res.recipes
+          this.setState({ saved })})
   }
 
   componentWillMount(){
@@ -28,7 +104,7 @@ class Saved extends Component {
           <div>
           {this.state.saved.length != 0 &&
             <div>
-            <SavedRecipes saved={this.state.saved}/>
+            <SavedRecipes show={this.state.show} ids={this.state.ids} handleShow={this.handleShow} handleClose={this.handleClose} handleClear={this.handleClear} handleAdd={this.handleAdd} handleRemove={this.handleRemove} handleDelete={this.handleDelete} saved={this.state.saved}/>
             </div>
           }
           </div>
